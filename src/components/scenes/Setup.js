9432eda989e79b4b0bf1b28cb3e3438db7791428
Marcus Hays <Marcus.hays@punchkick.com>
2017-11-17 17:07:46 -0600

    Rendering first component

---
+++ src/components/scenes/Setup.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+    View,
+    Text,
+} from 'react-native';
+
+const Setup = () => {
+    return (
+        <View> 
+            <Text> This is the Setup Page! </Text>
+        </View>
+    );
+}
+
+export default Setup;
